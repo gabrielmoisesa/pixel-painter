914dcf22d033d690b67a07ab3987e1de0e5276bd
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-05-18 20:34:55 -0300

    add req 5

--- script.js
+++ script.js
@@ -16,6 +16,28 @@ function addRandomColors() {
     document.getElementById('three').style.backgroundColor = color2;
     const color3 = getRandomColor();
     document.getElementById('four').style.backgroundColor = color3;
+
+    const colorPalette = {
+      color1,
+      color2,
+      color3,
+    };
+    localStorage.setItem('colorPalette', JSON.stringify(colorPalette));
   });
+const savedColorPalette = localStorage.getItem('colorPalette');
+
+if (savedColorPalette) {
+  const { color1, color2, color3 } = JSON.parse(savedColorPalette);
+
+  if (color1) {
+    document.getElementById('two').style.backgroundColor = color1;
+  }
+  if (color2) {
+    document.getElementById('three').style.backgroundColor = color2;
+  }
+  if (color3) {
+    document.getElementById('four').style.backgroundColor = color3;
+  }
+ }
 }
 addRandomColors();
